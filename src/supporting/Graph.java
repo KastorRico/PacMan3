77ff0510e42eb2c5ecb5fe3437e1d65a80a95e6d
Konstantin <59829955+KatrichKostiantin@users.noreply.github.com>
2020-11-03 19:16:24 +0200

    Delete useless classes

--- src/supporting/Graph.java
+++ src/supporting/Graph.java
@@ -1,20 +1,12 @@
 package supporting;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Graph {
-
     private final int V;
     private Map<Point, Bag<Point>> adj;
 
-    /**
-     * Створюємо порожній граф розмірності V
-     * ініціалізуємо масив порожніми списками типу supporting.Bag
-     *
-     * @param V - кількість вершин
-     */
     public Graph(int V) {
         this.V = V;
         adj = new HashMap<>(V);
@@ -26,12 +18,6 @@ public class Graph {
         adj.putAll(graph.adj);
     }
 
-    /**
-     * додаємо ребро між двома вершинами
-     *
-     * @param v - вершина
-     * @param w - вершина
-     */
     public void addEdge(Point v, Point w) {
         if (adj.get(v) == null)
             adj.put(v, new Bag<>());
@@ -41,12 +27,6 @@ public class Graph {
         adj.get(w).add(v);
     }
 
-    /**
-     * ітератор по вершинам суміжним з вершиною v
-     *
-     * @param v - вершина
-     * @return - ітератор по мішку суміжних з v вершин
-     */
     public Iterable<Point> adj(Point v) {
         return adj.get(v);
     }
