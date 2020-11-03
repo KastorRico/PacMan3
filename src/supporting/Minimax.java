a6cac1e904d69b62fb4053c6e4e5e94d2d5effa4
KovalOleg <oleh.koval@ukma.edu.ua>
2020-11-03 21:51:31 +0200

    Beta Minimax

--- src/supporting/Minimax.java
+++ src/supporting/Minimax.java
@@ -1,45 +1,49 @@
 package supporting;
-
-import javafx.scene.Node;
-
 import java.util.ArrayList;
 
 public class Minimax {
-    private Tree tree;
     private final int GLOBALDEPTH = 4;
     private ArrayList<Node> result = new ArrayList<>();
 
-    public Minimax(){}
+    public Minimax() {
+    }
 
-    public ArrayList<Node> findStrategy(Tree tree){
-        helpToFind(GLOBALDEPTH,tree.root);
+    public ArrayList<Node> findStrategy(Tree tree) {
+        helpToFind(GLOBALDEPTH, tree.tree, true);
         ArrayList<Node> copyResult = new ArrayList<>();
-        copyResult.addAll(result);
+        for(int i = result.size();i>=0;i--)
+            copyResult.add(result.get(i));
         result.clear();
         return copyResult;
     }
 
-    public Node helpToFind(int depth, Node node){
-        /*
-        if depth = 0 or node is a terminal node then
-            return the heuristic value of node
-        if maximizingPlayer then
-          value := −∞
-          for each child of node do
-            value := max(value, minimax(child, depth − 1, FALSE))
-          return value
-        else (* minimizing player *)
-            value := +∞
-            for each child of node do
-                value := min(value, minimax(child, depth − 1, TRUE))
-            return value
-        */
+    private int helpToFind(int depth, Node node, boolean maxPlay) {
         Node root = node;
-        if(node.list)
-        if (root.value != null){
-            return root.value;
-        } else
-            root.value = minimumValueOfNodes();
-        return null;
+        if(depth == 0) return node.value;
+        if (maxPlay) {
+            node.value = Integer.MIN_VALUE;
+            for(Node n:node.list){
+                node.value = max(node.value,helpToFind(depth - 1, n, true));
+            }
+            result.add(node);
+
+        } else {
+            node.value = Integer.MAX_VALUE;
+            for(Node n:node.list){
+                node.value = min(node.value,helpToFind(depth - 1, n, true));
+            }
+            result.add(node);
+        }
+        return 0;
+    }
+
+    private int min(int value1, int value2) {
+        if (value1<value2) return value1;
+        else return value2;
+    }
+
+    private int max(int value1, int value2) {
+        if (value1>value2) return value1;
+        else return value2;
     }
 }
