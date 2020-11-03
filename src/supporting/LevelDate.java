db64c5900ea60b37b5d3be9eb2eb33b3624baea8
Konstantin <59829955+KatrichKostiantin@users.noreply.github.com>
2020-11-03 23:31:29 +0200

    Add heuristic

--- src/supporting/LevelDate.java
+++ src/supporting/LevelDate.java
@@ -9,6 +9,16 @@ public class LevelDate {
 
     private Point pacmanLocation;
     private ArrayList<Point> ghostsLocation;
+    private int weight;
+    private short[][] data;
+    private int radius = 3;
+
+    public LevelDate(short[][] levelData, Point pacmanLocation, ArrayList<Point> ghostsLocation) {
+        data = levelData;
+        this.pacmanLocation = pacmanLocation;
+        this.ghostsLocation = ghostsLocation;
+        weight = heuristic();
+    }
 
     public int getWeight() {
         return weight;
@@ -18,17 +28,26 @@ public class LevelDate {
         this.weight = weight;
     }
 
-    private int weight;
-    private short[][] data;
+    private int heuristic() {
+        int val = 0;
+        val += -1 * distanceToGhost(ghostsLocation.get(0));
+        val += -1 * distanceToGhost(ghostsLocation.get(1));
 
-    public LevelDate(short[][] levelData, Point pacmanLocation, ArrayList<Point> ghostsLocation) {
-        data = levelData;
-        this.pacmanLocation = pacmanLocation;
-        this.ghostsLocation = ghostsLocation;
-        weight = heuristic();
+        val += countOfDotsAroundPacman();
+        return val;
     }
 
-    private int heuristic() {
-        return 0;
+    private int countOfDotsAroundPacman() {
+        int val = 0;
+
+        for (int i = pacmanLocation.x - radius; i <= pacmanLocation.x + radius; i++)
+            for (int j = pacmanLocation.y - radius; j <= pacmanLocation.y + radius; j++)
+                if (i >= 0 && i < data.length && j >= 0 && j < data.length)
+                    if (data[i][j] % 16 == 0) val++;
+        return val;
+    }
+
+    private int distanceToGhost(Point ghost) {
+        return Math.abs(pacmanLocation.x - ghost.x) + Math.abs(pacmanLocation.y - ghost.y);
     }
 }
